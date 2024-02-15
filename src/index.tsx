cb32b4d98f4ed89fd72af11b3e091e7ee802f379
ty44 <y.takahashi424@gmail.com>
2024-02-15 20:36:09 +0900

    📦 feat: add biome.json configuration file for biomejs 🔧 chore: add @biomejs/biome to devDependencies in package.json 🎨 style(index.tsx): apply biomejs formatter to improve code readability and consistency

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,33 @@
-import { Hono } from 'hono'
-import { v4 as uuidv4 } from 'uuid'
+import { Hono } from "hono";
+import { v4 as uuidv4 } from "uuid";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.render(
-    <>
-      <script src="https://unpkg.com/htmx.org@1.9.10"></script>
-      <h1>Hello world</h1>
-      <p>
-        The response is: <span id="message" hx-get="/api/message" hx-trigger="load" hx-swap="innerText"></span>.
-      </p>
-    </>
-  )
-})
+app.get("/", (c) => {
+	return c.render(
+		<>
+			<script src="https://unpkg.com/htmx.org@1.9.10" />
+			<h1>Hello world</h1>
+			<p>
+				The response is:{" "}
+				<span
+					id="message"
+					hx-get="/api/message"
+					hx-trigger="load"
+					hx-swap="innerText"
+				/>
+				.
+			</p>
+		</>,
+	);
+});
 
-app.get('/api/message', (c) => {
-  return c.json({
-    message: 'Hello, World!',
-    id: uuidv4(),
-    date: new Date().toUTCString(),
-  })
-})
+app.get("/api/message", (c) => {
+	return c.json({
+		message: "Hello, World!",
+		id: uuidv4(),
+		date: new Date().toUTCString(),
+	});
+});
 
-export default app
+export default app;
